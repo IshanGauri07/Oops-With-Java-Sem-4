9fdc676457c5ac81194fc4fb25f1ae6f3f153993
ishan gauri <ishangauri506@gmail.com>
2026-01-15 20:58:10 +0530

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,9 @@
+public class Example1 {
+     public static void main(String[] args){
+          int a = 10;
+          int b = 0;
+          if (b != 0 & a/b > 2) {
+               System.out.println("valid");
+          }
+      }
+}
